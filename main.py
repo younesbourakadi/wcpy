7c0173e47d14cc2c33acc747603b7c88be7e85ba
Younes Bourakadi <younes.bourakadi@gmail.com>
2024-01-01 22:52:15 +0100

    Add argparse parser

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
+import argparse
+
 
 def wcpy(file_name):
+    parser = argparse.ArgumentParser(
+                    prog='pywc',
+                    description='unix wc written in python',
+                    epilog='Text at the bottom of help')
+
     file_stats = os.stat(file_name)
     return str(file_stats.st_size) + " " + file_name
+
 
 print(wcpy('test.txt'))
